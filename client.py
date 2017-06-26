23aea7774a6f02c969b2a8c6d51472584d70e696
Rafael Ning <rafaelning@gmail.com>
2017-06-26 15:52:50 -0700

    Added client.py and server.py files.

---
+++ client.py
@@ -0,0 +1,20 @@
+import asyncore, socket
+
+class Client(asyncore.dispatcher_with):
+
+     def __init__(self, host, port, message):
+        asyncore.dispatcher.__init__(self)
+        self.createsocket(socket.AF_INET, socket.SOCK_STREAM)
+        self.connect((host, port))
+        self.outBuffer = message
+
+    def handle_close(self):
+        print "Client: Connection Closed"
+        self.close()
+
+    def handle_read(self):
+        print "Received: ", self.recv(1024)
+        self.close()
+
+c = Client("", 8080, "Hello, !!!")
+asyncore.loop()
---
+++ server.py
@@ -0,0 +1,17 @@
+import asyncore, socket
+
+class Server(asyncore.dispatcher):
+    def __init__(self, host, port):
+        asycore.dispatcher.__init__(self)
+        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.bind(("", port))
+        self.listen(1)
+
+    def handle_accept(self):
+        socket, address = self.accept()
+        print "Server: Connection by ", address
+        socket.send(outBuffer)
+
+s = Server("", 8080)
+asyncore.loop()
+    
