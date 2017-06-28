41033ec4ba7b18d1e0348419c60831d3c89a1292
Rafael Ning <rafaelning@gmail.com>
2017-06-28 09:54:05 -0700

    Update server.py

--- server.py
+++ server.py
@@ -1,11 +1,13 @@
 import asyncore, socket
 
 class Server(asyncore.dispatcher):
-    def __init__(self, host, port):
+    def __init__(self, host, port, message):
         asyncore.dispatcher.__init__(self)
         self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
         self.bind((host, port))
         self.listen(1)
+        self.outBuffer = message
+        print "Server: Waiting for commection..."
 
     def handle_accept(self):
         socket, address = self.accept()
@@ -13,6 +15,6 @@ class Server(asyncore.dispatcher):
         socket.send(outBuffer)
 
 add = input("Enter IP address of server in single quotes:\n")
-s = Server(add, 8080)
+s = Server(add, 8080, "Server says hello!!!")
 asyncore.loop()
     
