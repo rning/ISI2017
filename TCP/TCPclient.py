c608e410650f8935f415e292766f397d1fdbe709
Rafael Ning <rafaelning@gmail.com>
2017-07-06 15:33:06 -0700

    outBuffer

--- TCP/TCPclient.py
+++ TCP/TCPclient.py
@@ -2,10 +2,11 @@ import asyncore, socket
 
 class Client(asyncore.dispatcher_with_send):
 
-    def __init__(self, host, port):
+    def __init__(self, host, port, message):
         asyncore.dispatcher.__init__(self)
         self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
         self.connect((host, port))
+        self.outBuffer = message
 
     def handle_close(self):
         print "Client: Connection Closed"
