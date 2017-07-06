800245a0db38d64f6921040e0663fe840a6a190b
Rafael Ning <rafaelning@gmail.com>
2017-07-06 15:42:34 -0700

    outBuffer

---
+++ TCP/ParameterParser.py
@@ -0,0 +1,13 @@
+parameters = open("./Parameters.txt")
+parameterVariables = list()
+varDict = dict()
+
+for line in parameters:
+    parameterVariables = line.split(": ")
+    tempVar = parameterVariables[0]
+    tempVal = parameterVariables[1].replace("\n", "")
+    varDict[tempVar] = tempVal
+
+print varDict
+
+# refer to variables with varDict["variableName"]
---
+++ TCP/TCPclient.py
@@ -0,0 +1,29 @@
+import asyncore, socket
+
+class Client(asyncore.dispatcher_with_send):
+
+    def __init__(self, host, port, message):
+        asyncore.dispatcher.__init__(self)
+        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.connect((host, port))
+        self.outBuffer = message
+
+    def handle_close(self):
+        print "Client: Connection Closed"
+        self.close()
+
+    def readable(self):
+        return True
+
+    def handle_read(self):
+        print "Received: ", self.recv(1024)
+
+    def writeable(self):
+        return bool(self.outBuffer)
+
+    def handle_write(self):
+        self.send(self.outBuffer)
+
+add = input("Enter IP address of server in single quotes:\n")
+c = Client(add, 8080)
+asyncore.loop()
