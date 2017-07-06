4bc945072f1eed969c06166d14fcd0f532eb6cb5
M888R <manas@shahparivar.info>
2017-07-06 15:27:54 -0700

    dfasdfasdf
    dfsadfasdfs

--- TCP/TCPclient.py
+++ TCP/TCPclient.py
@@ -21,7 +21,7 @@ class Client(asyncore.dispatcher_with_send):
         return bool(self.outBuffer)
 
     def handle_write(self):
-        socket.send(self.outBuffer)
+        self.send(self.outBuffer)
 
 add = input("Enter IP address of server in single quotes:\n")
 c = Client(add, 8080)
