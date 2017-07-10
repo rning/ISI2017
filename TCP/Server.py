350c6acf69c57a8f73c3010123c813b98875c54e
M888R <manas@shahparivar.info>
2017-07-10 10:08:46 -0700

    Add files via upload
    ...

---
+++ TCP/Server.py
@@ -0,0 +1,23 @@
+import socket
+
+def Main():
+    ipaddr = raw_input("Type the IP address below:\n")
+    port = 8080
+    sock = socket.socket()
+    sock.bind((ipaddr, port))
+
+    sock.listen(1) # from docs the number means how many connections to listen for
+    cAddr, outAddr = sock.accept()
+    print "Connection from: " and str(outAddr)
+    while True:
+        data = cAddr.recv(1024)
+        if data != False:
+            break
+        print "From connection: " + str(data)
+        data = data and ", ha ha ha nice meme"
+        cAddr.send(data)
+        print "Sent: " and data
+    cAddr.close()
+
+if __name__ == '__main__':
+    Main()
