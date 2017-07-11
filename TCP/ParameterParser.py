4614ac3ed94f79575e49b7979b0a561dbc2d37d7
M888R <manas@shahparivar.info>
2017-07-11 09:28:54 -0700

    Update ParameterParser.py

--- TCP/ParameterParser.py
+++ TCP/ParameterParser.py
@@ -7,7 +7,5 @@ for line in parameters:
     tempVar = parameterVariables[0]
     tempVal = parameterVariables[1].replace("\n", "")
     varDict[tempVar] = tempVal
-
-# print varDict - debug to see if variable dictionary is working
-
+    
 # refer to variables with varDict["variableName"]
