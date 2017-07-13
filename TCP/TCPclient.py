10ac8d62fb80818a288c439267d5b1262b24ee6f
Rafael Ning <rafaelning@gmail.com>
2017-07-12 18:41:17 -0700

    test

--- TCP/TCPclient.py
+++ TCP/TCPclient.py
@@ -35,7 +35,7 @@ class Client(asyncore.dispatcher):
         #send ACK immediately for each pack recieved (presumably within handle_read, but maybe with handle_write)
         if recPack[0] == (self.packSeq + 1):
             self.packSeq += 1
-            self.send(struct.pack('LL', self.packSeq))#send here or in handle_write?
+            self.send(struct.pack('LL', self.packSeq, 0))#send here or in handle_write?
 
     def writable(self):
         print "Writable -> ", bool(self.outBuffer and self.isConnected)
