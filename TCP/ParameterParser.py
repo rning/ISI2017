023fb9e1ae80ad39989cb83f9157475f792d0ea7
M888R <manas@shahparivar.info>
2017-07-15 00:25:24 -0700

    fixed parameterparser
    importing should work properly now

--- TCP/ParameterParser.py
+++ TCP/ParameterParser.py
@@ -1,11 +1,15 @@
-parameters = open("./Parameters.txt")
-parameterVariables = list()
-varDict = dict()
+def parameter(pname):
+    parameters = open("./Parameters.txt")
+    parameterVariables = list()
+    varDict = dict()
 
-for line in parameters:
-    parameterVariables = line.split(": ")
-    tempVar = parameterVariables[0]
-    tempVal = parameterVariables[1].replace("\n", "")
-    varDict[tempVar] = tempVal
-    
-# refer to variables with varDict["variableName"]
+    for line in parameters:
+        parameterVariables = line.split(": ")
+        tempVar = parameterVariables[0]
+        tempVal = parameterVariables[1].replace("\n", "")
+        varDict[tempVar] = tempVal
+    return varDict[pname]
+# refer to variables with parameter({parameter}), which will return the parameter's value.
+
+if __name__ == "__main__":
+    print parameter(data)
