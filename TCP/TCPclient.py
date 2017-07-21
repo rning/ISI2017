5b180e73f4b44ce61e536127c659daaceb6f422f
Rafael Ning <rafaelning@gmail.com>
2017-07-21 14:31:17 -0700

    test

--- TCP/TCPclient.py
+++ TCP/TCPclient.py
@@ -18,21 +18,24 @@ class Client(asyncore.dispatcher):
 
     def handle_read(self): #split up buffer
         print "handle_read reading..."
-        
-        #unpack structure sent from server: [seq,ack,string]
-        packet = struct.unpack('LL24s', self.recv(4096)) #size: 32 bytes
-        #debug
-        print 'received packet with seq#' , packet[0], 'ack#', packet[1] 
-        
-        #increment ACK based on receieved pack seq
-        #send ACK immediately for each pack recieved
-        if packet[0] == (self.ack + 1):
-            self.ack += 1
-            #pack structure and send to server: [seq,ack,string]
-            self.send(struct.pack('LL24s', self.seq, self.ack, 'data'))
 
-            #debug
-            print 'sending ack'
+        readBuffer = self.recv(4096)
+        for i in rance(0, len(readBuffer) / 40):
+            #unpack structure sent from server: [seq,ack,string]
+            packet = struct.unpack('LL24s', readBuffer[:41]) #size: 32 bytes
+
+            print 'received packet with seq#' , packet[0], 'ack#', packet[1] 
+
+            #increment ACK based on receieved pack seq
+            #send ACK immediately for each pack recieved
+            if packet[0] == (self.ack + 1):
+                self.ack += 1
+                #pack structure and send to server: [seq,ack,string]
+                self.send(struct.pack('LL24s', self.seq, self.ack, 'data'))
+
+                print 'sending ack'
+
+            readBuffer = readBuffer[41:]
 
 if __name__ == '__main__':
 
